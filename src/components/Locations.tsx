7446a79d201c8d359c00722a79f7f6c358ed5946
Radomila Hejhalová <hejhalova.radomila@gmail.com>
2024-02-01 17:07:32 +0100

    fix: Locations component separation

--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -15,47 +15,49 @@ import AlertBox from './AlertBox';
 function Locations({ input }: { input: string }) {
   const { code, loading } = useCode(input);
 
-  return (
-    <>
-      {code ? (
-        code?.places?.length > 0 ? (
-          <Card width="60rem">
-            <CardHeader>
-              <Heading size="md">List of places</Heading>
-            </CardHeader>
-            <CardBody>
-              {!loading ? (
-                <Stack divider={<StackDivider />} spacing="4">
-                  {code?.places?.map((place) => {
-                    return (
-                      <Box>
-                        <Text pt="2" fontSize="lg">
-                          {place['place name']}
-                        </Text>
-                      </Box>
-                    );
-                  })}
-                </Stack>
-              ) : (
-                <Spinner />
-              )}
-            </CardBody>
-          </Card>
-        ) : (
-          <AlertBox
-            status="error"
-            title="No places found."
-            desc="We are sorry, but for this zip code wasn't found any places."
-          />
-        )
-      ) : (
-        <AlertBox
-          status="info"
-          desc="In order to show the list of places, please enter the zip code."
-        />
-      )}
-    </>
-  );
+  if (!code) {
+    return (
+      <AlertBox
+        status="info"
+        desc="In order to show the list of places, please enter the zip code."
+      />
+    );
+  }
+
+  if (code?.places?.length > 0) {
+    return (
+      <Card width="60rem">
+        <CardHeader>
+          <Heading size="md">List of places</Heading>
+        </CardHeader>
+        <CardBody>
+          {!loading ? (
+            <Stack divider={<StackDivider />} spacing="4">
+              {code?.places?.map((place) => {
+                return (
+                  <Box>
+                    <Text pt="2" fontSize="lg">
+                      {place['place name']}
+                    </Text>
+                  </Box>
+                );
+              })}
+            </Stack>
+          ) : (
+            <Spinner />
+          )}
+        </CardBody>
+      </Card>
+    );
+  } else {
+    return (
+      <AlertBox
+        status="error"
+        title="No places found."
+        desc="We are sorry, but for this zip code wasn't found any places."
+      />
+    );
+  }
 }
 
 export default Locations;
